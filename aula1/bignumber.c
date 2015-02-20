5aa93231f3730a8d4ed87d6ad7fe765466bb88f4
unknown <raldney.salves@CAS51175710606.senacsp.edu.br>
2015-02-19 22:33:35 -0200

    aula19_02

--- aula1/bignumber.c
+++ aula1/bignumber.c
@@ -1,8 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include 'pilha.h'
+#include "../listasLigadas/pilha.c"
+
+
+int main(){
+    pilha *p1 = pilha_aloca();
+    pilha *p2 = pilha_aloca();
+    pilha *p3 = pilha_aloca();
+
+    printf("DIGITE A POHA DO NUMERO GRANDE!!!");
+    /*
+    int i = 0;
+    for(i = 0; i <= 10;i++)
+       pilha_insere(pilha,i);
 
 
 
+    for(i = 0; i <= 10;i++)
+        printf("%d",pilha_retira(pilha));
+    */
+
+    return 0;
+
+
+}
 
 
